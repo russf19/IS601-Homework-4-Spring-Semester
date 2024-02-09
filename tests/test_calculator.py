fd96f422bbcc9056baea6f191cc34735bc641b5c
russf19 <russ_f19@verizon.net>
2024-02-09 15:38:29 -0500

    fixed testing issues

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,12 +4,12 @@ from calculator import add, subtract, multiply, divide
 def test_addition():
     assert add(2,2) == 4
 
-def test_ subtraction():
+def test_subtraction():
     assert subtract(2,2) == 0
 
-def test_ multiplication():
+def test_multiplication():
     assert multiply(3,3) == 9
 
-def test_ division():
+def test_division():
     assert divide(6,2) == 3
 
